ffd9ec740063d0af2f3bde5adb3ad1744f11ff04
postbox433@gmail.com <postbox433@gmail.com>
2015-06-19 17:32:17 +0500

    fix soap service settings

--- lib/BaseServiceSoap.php
+++ lib/BaseServiceSoap.php
@@ -50,6 +50,16 @@ class BaseServiceSoap extends BaseService
 		}
 	}
 
+
+	/**
+	 * @param array $config
+	 */
+	public function config(array $options)
+	{
+		$this->_client = null;
+		return parent::config($options);
+	}
+
 	/**
 	 * @param string $method
 	 * @param array $params
@@ -90,15 +100,25 @@ class BaseServiceSoap extends BaseService
 		if (!empty($this->wsdl) && $this->_client === null) {
 			if ($this->catchExceptions) {
 				try {
-					$this->_client = new SoapClient($this->wsdl, $this->soapOptions);
+					$this->_client = $this->createSoapClient($this->wsdl, $this->soapOptions);
 				} catch (Exception $e) {
 					$this->addError($e->getMessage());
 				}
 			} else {
-				$this->_client = new SoapClient($this->wsdl, $this->soapOptions);
-			}			
+				$this->_client = $this->createSoapClient($this->wsdl, $this->soapOptions);
+			}
 		}
 		return $this->_client;
+	}
+
+	/**
+	 * @param string $wsdl
+	 * @param array $soapOptions
+	 * @return \SoapClient
+	 */
+	protected function createSoapClient($wsdl, array $soapOptions = array())
+	{
+		return new \SoapClient($wsdl, $soapOptions);
 	}
 
 	/**
